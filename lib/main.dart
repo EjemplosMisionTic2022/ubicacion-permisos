097c693fb32ce256505ee86f964ac7291a29f70e
Franklin E. Zabaleta <franklinz@uninorte.edu.co>
2021-12-10 23:03:31 -0500

    new session

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:misiontic_template/domain/use_case/controllers/notifications.dart';
+import 'package:misiontic_template/domain/use_case/location_management.dart';
+import 'package:misiontic_template/domain/use_case/notification_management.dart';
 import 'package:misiontic_template/ui/app.dart';
+import 'package:workmanager/workmanager.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  NotificationController notificationController =
+      Get.put(NotificationController());
+  await notificationController.initialize();
+  notificationController.createChannel(
+      id: 'user-location',
+      name: 'User Location',
+      description: 'My Location...');
+  await Workmanager().initialize(
+    updatePositionInBackground,
+    isInDebugMode: false,
+  );
+  await Workmanager().registerPeriodicTask(
+      "1",
+      "locationPeriodicTask",
+    );
   runApp(const App());
+}
+
+void updatePositionInBackground() async {
+  final manager = LocationManager();
+  final _manager = NotificationManager();
+  Workmanager().executeTask((task, inputData) async {
+    await _manager.initialize();
+    final _channel = _manager.createChannel(
+        id: 'user-location',
+        name: 'User Location',
+        description: 'My Location...');
+    final position = await manager.getCurrentLocation();
+    _manager.showNotification(
+        channel: _channel,
+        title: 'Tu ubicación es...',
+        body: 'Latitud ${position.latitude} - Longitud: ${position.longitude}');
+    return Future.value(true);
+  });
 }
--- lib/ui/pages/stateful/screens/gps_screen.dart
+++ lib/ui/pages/stateful/screens/gps_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:misiontic_template/domain/use_case/controllers/location.dart';
+import 'package:misiontic_template/domain/use_case/controllers/notifications.dart';
 import 'package:misiontic_template/domain/use_case/controllers/permissions.dart';
 import 'package:misiontic_template/domain/use_case/location_management.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -17,6 +18,7 @@ enum RadioState { on, off }
 class _State extends State<GpsScreen> {
   late PermissionsController permissionsController;
   late LocationController locationController;
+  late NotificationController notificationController;
   late LocationManager manager;
 
   @override
@@ -24,6 +26,7 @@ class _State extends State<GpsScreen> {
     super.initState();
     permissionsController = Get.find();
     locationController = Get.find();
+    notificationController = Get.find();
     manager = LocationManager();
   }
 
@@ -41,8 +44,10 @@ class _State extends State<GpsScreen> {
                   if (permissionsController.locationGranted) {
                     final position = await manager.getCurrentLocation();
                     locationController.location.value = position;
-                    Get.snackbar('Tu ubicación es...',
-                        'Latitud ${position.latitude} - Longitud: ${position.longitude}');
+                    notificationController.show(
+                        title: 'Tu ubicación es...',
+                        body:
+                            'Latitud ${position.latitude} - Longitud: ${position.longitude}');
                   }
                 },
                 child: const Text('Obtener Ubicacion'),
